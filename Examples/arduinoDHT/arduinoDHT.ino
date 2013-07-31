e71c8b67d9620237c8eb6302c83556de66471f61
Jon Zobrist <jon@jonzobrist.com>
2013-07-31 17:32:55 -0600

    initial dht import
    https://github.com/markruys/arduino-DHT

---
+++ Examples/arduinoDHT/arduinoDHT.ino
@@ -0,0 +1,19 @@
+#include "DHT.h"
+
+DHT dht;
+
+void setup()
+{
+  Serial.begin(9600);
+
+  dht.setup(2); // data pin 2
+}
+
+void loop()
+{
+  delay(dht.getMinimumSamplingPeriod());
+
+  Serial.print(dht.getHumidity());
+  Serial.print("\t");
+  Serial.print(dht.getTemperature());
+}
